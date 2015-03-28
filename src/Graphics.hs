a48b19af510df4d5341357cac52b770ebbdba7da
Zachary Sarver <Zachary.Sarver@gmail.com>
2015-03-28 15:12:30 -0500

    Better types in Graphics

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -1,6 +1,7 @@
 --Graphics.hs
 module Graphics where
 
+import Types
 import Graphics.UI.SDL.Types
 import Graphics.UI.SDL.Video (renderClear, renderCopy, renderPresent)
 import Foreign.Ptr (Ptr(..))
@@ -11,11 +12,13 @@ import Control.Monad (sequence)
 untriple :: (a,b,c) -> ((a,b),c)
 untriple (x,y,z) = ((x,y),z)
 
-render :: Renderer -> [(Texture, Ptr Rect, Ptr Rect)] -> IO [CInt]
+renderTriple :: RenderObject -> ((Texture, Ptr Rect), Ptr Rect)
+renderTriple ro = ((texture ro, source ro), dest ro)
+
+render :: Renderer -> [RenderObject] -> IO [CInt]
 render r ts = do
   renderClear r
-  let rc = renderCopy r
-  let ts' = map untriple ts
-  let res = map (uncurry (uncurry rc)) ts' :: [IO CInt]
+  let ts' = map renderTriple ts    
+  let res = map (uncurry (uncurry (renderCopy r))) ts' :: [IO CInt]
   renderPresent r
-  return $ sequence res
+  (sequence res) >>= return
--- src/Types.hs
+++ src/Types.hs
@@ -3,6 +3,13 @@ module Types where
 import Vector
 import Keyboard (Key(..))
 import FRP.Helm.Color (rgba, Color, yellow, red, blue, green)
+import Graphics.UI.SDL.Types (Texture (..), Rect (..))
+import Foreign.Ptr (Ptr(..))
+
+data RenderObject = RenderObject { texture :: Texture
+                                 , source :: Ptr Rect
+                                 , dest :: Ptr Rect
+                                 }
 
 type Time = Double
 
